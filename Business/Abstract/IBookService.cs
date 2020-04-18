96aa80cffa3da08b42b97cc0fe5966f7115ca95c
ereninal10@gmail.com <ereninal10@gmail.com>
2020-04-18 22:59:33 +0300

    Solid buil created

---
+++ Business/Abstract/IBookService.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Abstract
+{
+    public interface IBookService
+    {
+        List<Book> GetAll();
+        Book GetById(int id);
+        List<Book> GetAuthorById(int id);
+        void Add();
+        void Delete();
+        void Update();
+
+    }
+}
